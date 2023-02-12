aa5b8ed2632af82565963a4b7ae9168b7c6041cf
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-02-12 11:38:56 +0100

    released

---
+++ dbcreator.js
@@ -0,0 +1,30 @@
+var prompt = require("prompt-sync")({ sigint: true });
+var fs = require('fs');
+var crypto = require('crypto');
+var os = require("os");
+
+// Set delay to ms
+function delay(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+const filetoadd = prompt("Enter path/name of file to be added: ");
+const buff = fs.readFileSync(filetoadd);
+var outputdata0 = crypto.createHash("md5").update(buff).digest("hex");
+var outputdata = outputdata0.toString().toUpperCase();
+
+var text = outputdata;
+processInput()
+
+function processInput () {     
+  fs.open('dbcreator.txt', 'a', 666, function( e, id ) {
+   fs.write( id, text + os.EOL, null, 'utf8', function(){
+    fs.close(id, function(){
+     console.log('Custom database is updated');
+     console.log('If the file is a virus, please submit the hash here:');
+     console.log('\x1b[32m%s\x1b[0m', "")
+    });
+   });
+  });
+ }
